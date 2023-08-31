eb8bf85816468c1711e058b842b485efc7d1d762
SilvioDiasJr <silviodiasjr@outlook.com>
2023-08-31 11:10:10 -0300

    feat: adding tina.io

---
+++ tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,47 @@
+// tina/config.ts
+import { defineConfig } from "tinacms";
+var branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+var config_default = defineConfig({
+  branch,
+  clientId: null,
+  // Get this from tina.io
+  token: null,
+  // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static"
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static"
+    }
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true
+          }
+        ]
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};
